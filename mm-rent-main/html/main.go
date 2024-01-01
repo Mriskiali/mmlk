21d9c3bb74797773d346d0dcb2c6b6f4326fedc9
Mriskiali <muafariskiali1805@gmail.com>
2024-01-02 06:53:28 +0700

    ke tujuh

---
+++ mm-rent-main/html/main.go
@@ -0,0 +1,25 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the root page!")
+}
+
+func handleHTML(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func main() {
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/html", handleHTML)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
